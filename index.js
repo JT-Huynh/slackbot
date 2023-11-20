86c1ada46f446144eb01716c547bb1803f8fb6c9
ngochuynh <huynhtransongngoc@gmail.com>
2023-11-20 16:56:30 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#! usr/bin/env  node
+import ora from 'ora';
+import psi from 'psi';
+
+(async () => {
+  // Get the PageSpeed Insights report
+  const loader = ora('Measuring URL').start();
+  const { data } = await psi('https://theverge.com');
+
+  if (data) loader.succeed();
+  console.log('Speed score:', data.lighthouseResult.categories.performance.score);
+
+  // Output a formatted report to the terminal
+  await psi.output('https://theverge.com');
+  console.log('Done');
+})();
